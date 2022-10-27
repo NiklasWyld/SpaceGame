2a13abb064f9e1b796dfc881e29158cd70628e15
Niklas <84245129+NiklasWyld@users.noreply.github.com>
2022-10-27 17:13:59 +0200

    Add files via upload

---
+++ src/de/soeman/game/Entity.java
@@ -0,0 +1,9 @@
+package de.soeman.game;
+
+public class Entity{
+    public int x;
+    public int y;
+    public int size;
+
+    public int speed;
+}
---
+++ src/de/soeman/game/Obstacle.java
@@ -0,0 +1,50 @@
+package de.soeman.game;
+
+import java.awt.*;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Obstacle extends Entity {
+    GamePanel panel;
+    Shape obstacle;
+    Player player;
+
+    public Obstacle(GamePanel panel, Player player) {
+        this.panel = panel;
+        this.player = player;
+
+        x = 750;
+        y = ThreadLocalRandom.current().nextInt(1, 550 + 1);
+
+        size = 32;
+        speed = 2;
+    }
+
+    public boolean checkForCollision(Shape a, Shape b) {
+        Area areaA = new Area(a);
+        areaA.intersect(new Area(b));
+        return !areaA.isEmpty();
+    }
+
+    public void update() {
+        if (x <= 0) {
+           panel.createNewObstacle();
+        }
+
+        x -= speed;
+
+        obstacle = new Ellipse2D.Double(x, y, size, size);
+
+        if(checkForCollision(player.shape, obstacle)) {
+            Game.gameOver();
+        }
+    }
+
+    public void draw(Graphics2D g) {
+        obstacle = new Ellipse2D.Double(x, y, size, size);
+        g.setPaint(Color.red);
+        g.fill(obstacle);
+        g.draw(obstacle);
+    }
+}
